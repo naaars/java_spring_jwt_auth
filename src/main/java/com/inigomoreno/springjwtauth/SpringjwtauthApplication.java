e8a32b02c062eced3482d3df0390b89c5c0b3c83
Inigo Moreno <inigomorenotato@gmail.com>
2024-03-25 17:05:52 +0100

    Initial commit

---
+++ src/main/java/com/inigomoreno/springjwtauth/SpringjwtauthApplication.java
@@ -0,0 +1,13 @@
+package com.inigomoreno.springjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtauthApplication.class, args);
+	}
+
+}
